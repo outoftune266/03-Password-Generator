bec29bfe6afc3c564ad2b1cc1e9ca6319a93964a
Zak Monnet <zakmonnet@gmail.com>
2020-08-18 11:13:07 -0500

    rebuilt javascript to properly combine arrays

--- script.js
+++ script.js
@@ -13,23 +13,34 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-let lowerCase = ["a", "b" ,"c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-let upperCase = ["A", "B" ,"C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-let specialChar = ["!", "#", "$", "%", "&", "*", "+", "-", ":", ";", "<", ">", "=", "?", "@", "^", "_", "~"]
-var final = []
 
-function criteria() {
-  var passwordLegnth = prompt("How long should your password be? (Any length between 8 and 128 characters is valid")
+const lowerCase = ["a", "b" ,"c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const upperCase = ["A", "B" ,"C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const specialChar = ["!", "#", "$", "%", "&", "*", "+", "-", ":", ";", "<", ">", "=", "?", "@", "^", "_", "~"];
+let final = [];
+let password = "";
+
+// Builds array 
+function writePassword() {
+  let passwordLegnth = prompt("How long should your password be? (Any length between 8 and 128 characters is valid");
   if (passwordLegnth >= 8 && passwordLegnth <= 128){
-    if (alert("Do you want to include lowercase letter? Click OK for YES and Cancel for NO")) {
-      final.concat(lowerCase)
-    }
+    if (confirm("Do you want to include lowercase letters? Click OK for YES and Cancel for NO")) {
+      final = final.concat(lowerCase);
+    } if (confirm("Do you want to include UPPERCASE letters? Click OK for YES and Cancel for NO")) {
+      final = final.concat(upperCase);
+    } if (confirm("Do you want to include numbers? Click OK for YES and Cancel for NO")) {
+      final = final.concat(numbers);
+    } if (confirm("Do you want to include special characters? Click OK for YES and Cancel for NO")) {
+      final = final.concat(numbers);
+    } 
     console.log(final)
+    for (i = 0; i < passwordLegnth; i++) {
+      password = password + final[Math.floor(Math.random() * final.length)]
+
+    } console.log(password)
   } else {
-    alert("Please try again and enter a number between 8 and 128")
+    alert("Please try again and enter a number between 8 and 128");
   }
 }
 
-console.log(final)
-console.log(lowerCase)
